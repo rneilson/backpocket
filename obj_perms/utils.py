d3a5751a739666981dd39ea24a57d42ff7125cd8
Raymond Neilson <rneilson@interloper.ca>
2017-04-11 12:34:22 -0400

    Move split_perm() into module-level func, move label prepending to available_permissions()

--- obj_perms/utils.py
+++ obj_perms/utils.py
@@ -3,9 +3,10 @@
 from django.contrib.auth import get_permission_codename
 
 # Easier to do what 'migrate' does than fetch from db
-def available_permissions(model):
+def available_permissions(model, prepend_label=False):
     """
-    Gets valid permissions for model. Does *not* prepend app label.
+    Gets valid permissions for model. Prepends app label if
+    prepend_label is True.
     """
     meta = model._meta
     perms = set(
@@ -13,8 +14,38 @@ def available_permissions(model):
         for action in meta.default_permissions
     )
     perms.update(name for name, _ in meta.permissions)
+    if prepend_label:
+        app_label = model._meta.app_label
+        perms = set('{0}.{1}'.format(app_label, perm) for perm in perms)
+
     # TODO: cache available permissions per-model
     return perms
+
+def split_perm(model, perm, check_valid=False, check_list=None):
+    """
+    Ensures perm is a valid permission for this model.
+    Returns (app_label, codename) tuple.
+    """
+    model_app = model._meta.app_label
+    app_label, codename = perm.split('.', maxsplit=1)
+
+    if app_label != model_app:
+        raise ValueError(
+            "Permission '{0}' doesn't belong to app '{1}'"
+            .format(perm, model_app)
+        )
+
+    if check_valid:
+        if not check_list:
+            check_list = available_permissions(model)
+        if codename not in check_list:
+            raise ValueError(
+                "Permission '{0}' not valid for app '{1}'"
+                .format(perm, model_app)
+            )
+
+    # TODO: check cached available permissions
+    return app_label, codename
 
 
 class ObjectPermissionsBase(object):
@@ -30,26 +61,10 @@ class ObjectPermissionsBase(object):
         self.obj_perms = getattr(model, class_name)
 
     def _split_perm(self, perm):
-        """
-        Ensures perm is a valid permission for this model.
-        Returns (app_label, codename) tuple.
-        """
-        app_label, codename = perm.split('.', maxsplit=1)
+        return split_perm(self.model, perm)
 
-        if app_label != self.app_label:
-            raise ValueError(
-                "Permission '{0}' doesn't belong to app '{1}'"
-                .format(perm, self.app_label)
-            )
-
-        # TODO: check cached available permissions
-        return app_label, codename
-
-    def get_available_permissions(self):
+    def get_available_permissions(self, prepend_label=True):
         """
         Gets valid permissions for model, including app label.
         """
-        return set(
-            '{0}.{1}'.format(self.app_label, perm)
-            for perm in available_permissions(self.model)
-        )
+        return available_permissions(self.model, prepend_label)
