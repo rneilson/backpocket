8e73ad09297ba45f3abe3a23abf8c96f76e91d16
Raymond Neilson <rneilson@interloper.ca>
2017-04-11 23:55:12 -0400

    Remove ObjectPermissionsBase, rework split_perm()

--- obj_perms/utils.py
+++ obj_perms/utils.py
@@ -15,28 +15,31 @@ def available_permissions(model, prepend_label=False):
     )
     perms.update(name for name, _ in meta.permissions)
     if prepend_label:
-        app_label = model._meta.app_label
+        app_label = meta.app_label
         perms = set('{0}.{1}'.format(app_label, perm) for perm in perms)
 
     # TODO: cache available permissions per-model
     return perms
 
-def split_perm(model, perm, check_valid=False, check_list=None):
+def split_perm(model, perm, check_list=False):
     """
     Ensures perm is a valid permission for this model.
     Returns (app_label, codename) tuple.
     """
-    model_app = model._meta.app_label
-    app_label, codename = perm.split('.', maxsplit=1)
+    try:
+        app_label, codename = perm.split('.', maxsplit=1)
+    except ValueError:
+        app_label, codename = None, perm
 
-    if app_label != model_app:
+    model_app = model._meta.app_label
+    if app_label and app_label != model_app:
         raise ValueError(
             "Permission '{0}' doesn't belong to app '{1}'"
             .format(perm, model_app)
         )
 
-    if check_valid:
-        if not check_list:
+    if check_list:
+        if check_list is True:
             check_list = available_permissions(model)
         if codename not in check_list:
             raise ValueError(
@@ -47,24 +50,3 @@ def split_perm(model, perm, check_valid=False, check_list=None):
     # TODO: check cached available permissions
     return app_label, codename
 
-
-class ObjectPermissionsBase(object):
-    """
-    Base class for object permission testers/checkers.
-    """
-
-    def __init__(self, model, class_name):
-        self.model = model
-        self.app_label = model._meta.app_label
-        # Get object permissions nested class from model
-        # Let AttributeError bubble
-        self.obj_perms = getattr(model, class_name)
-
-    def _split_perm(self, perm):
-        return split_perm(self.model, perm)
-
-    def get_available_permissions(self, prepend_label=True):
-        """
-        Gets valid permissions for model, including app label.
-        """
-        return available_permissions(self.model, prepend_label)
