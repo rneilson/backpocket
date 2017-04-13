1263ac6b935eaba18c9512e939ae8fdae8df0e58
Raymond Neilson <rneilson@interloper.ca>
2017-04-13 01:38:23 -0400

    Add user is_authenticated check to ObjectPermissionsBackend

--- obj_perms/backends.py
+++ obj_perms/backends.py
@@ -11,12 +11,21 @@ class ObjectPermissionsBackend:
 
     DEFAULT_PERMISSION = False
     DEFAULT_ATTR_NAME = None
+    ALLOW_ANONYMOUS_USER = False
+
+    def _check_user(self, user_obj):
+        if not user_obj:
+            return False
+        return (user_obj.is_authenticated or self.ALLOW_ANONYMOUS_USER)
 
     def authenticate(self, request, **kwargs):
         # Never authenticate through this backend
         return None
     
     def has_perm(self, user_obj, perm, obj=None):
+        if not self._check_user(user_obj):
+            return False
+
         kwargs = { 'default': self.DEFAULT_PERMISSION }
 
         if self.DEFAULT_ATTR_NAME is not None:
@@ -25,6 +34,9 @@ class ObjectPermissionsBackend:
         return has_obj_perm(user_obj, perm, obj, **kwargs)
 
     def get_all_permissions(self, user_obj, obj=None):
+        if not self._check_user(user_obj):
+            return set()
+
         kwargs = { 'default': self.DEFAULT_PERMISSION }
 
         if self.DEFAULT_ATTR_NAME is not None:
