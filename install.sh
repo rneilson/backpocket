f8ce42117d55d6d370fcb4b7c36e6b217b741dbf
Raymond Neilson <rneilson@interloper.ca>
2017-04-23 15:16:09 -0400

    Start install script

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Change to base directory
+cd -P -- "$(dirname -- "$0")"
+# printf '%s\n' "$(pwd -P)"
+
+# Make data directory if not present
+mkdir -p "data"
+
+# TODO: create virtualenv
+
+# TODO: enter virtualenv
+
+# TODO: install dependencies
+
+# TODO: run initialization
