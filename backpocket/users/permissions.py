e86a4ad424b52a593442c3f6f6acbadaef579cc7
Raymond Neilson <rneilson@interloper.ca>
2017-04-24 12:25:33 -0400

    Add user-specific versions of BaseActionObjectPermissions, BaseActionObjectPermissionFilter

---
+++ backpocket/users/permissions.py
@@ -0,0 +1,40 @@
+from backpocket.permissions import (
+    BaseActionObjectPermissions, BaseActionObjectPermissionFilter
+)
+
+
+class UserObjectPermissions(BaseActionObjectPermissions):
+    """
+    BaseActionObjectPermissions updated with additional actions.
+    """
+    perms_map = {
+        **BaseActionObjectPermissions.perms_map,
+        'activate': (),
+        'password': (),
+        'reset_password': (),
+        'groups': (),
+        'permissions': (),
+    }
+
+    obj_perms_map = {
+        **BaseActionObjectPermissions.perms_map,
+        'activate': ('{app_label}.set_user_active',),
+        'password': ('{app_label}.set_user_password',),
+        'reset_password': ('{app_label}.reset_user_password',),
+        'groups': ('{app_label}.change_user_groups',),
+        'permissions': ('{app_label}.change_user_permissions',),
+    }
+
+
+class UserObjectPermissionFilter(BaseActionObjectPermissionFilter):
+    """
+    BaseActionObjectPermissionFilter updated with additional actions.
+    """
+    perms_map = {
+        **BaseActionObjectPermissionFilter.perms_map,
+        'activate': (),
+        'password': (),
+        'reset_password': (),
+        'groups': (),
+        'permissions': (),
+    }
