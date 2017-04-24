2a9c2c2a6b9c5370a9c0ade9701396d964a36e8f
Raymond Neilson <rneilson@interloper.ca>
2017-04-24 12:32:37 -0400

    Add UserSerializer

---
+++ backpocket/users/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from backpocket.users.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            'id', 'username', 'name', 'email',
+            'is_active', 'is_staff', 'date_joined',
+        )
