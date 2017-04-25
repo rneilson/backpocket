cd132e40810d1771760089f8e2255219fd1fb253
Raymond Neilson <rneilson@interloper.ca>
2017-04-24 23:03:50 -0400

    Make date_joined read-only

--- backpocket/users/serializers.py
+++ backpocket/users/serializers.py
@@ -9,3 +9,7 @@ class UserSerializer(serializers.ModelSerializer):
             'id', 'username', 'name', 'email',
             'is_active', 'is_staff', 'date_joined',
         )
+        read_only_fields = ('date_joined',)
+
+
+# TODO: Group serializer
