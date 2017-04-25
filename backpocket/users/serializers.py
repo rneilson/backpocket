ab36905445fbfa631aa1be45ac193af5bb689615
Raymond Neilson <rneilson@interloper.ca>
2017-04-24 23:30:20 -0400

    Add 'url' field to UserSerializer; make 'is_active' read-only (detail view to come)

--- backpocket/users/serializers.py
+++ backpocket/users/serializers.py
@@ -3,13 +3,15 @@ from backpocket.users.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name='user-detail')
+
     class Meta:
         model = User
         fields = (
             'id', 'username', 'name', 'email',
-            'is_active', 'is_staff', 'date_joined',
+            'is_active', 'is_staff', 'date_joined', 'url',
         )
-        read_only_fields = ('date_joined',)
+        read_only_fields = ('date_joined', 'is_active', 'url')
 
 
 # TODO: Group serializer
